59fa748ec8c316376282c836d6499e0407b8968b
TheMohawkNinja <themohawkninja@aol.com>
2023-05-29 15:05:30 -0400

    Added echos and roll.cpp

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
+echo "Compiling main.cpp..."
 g++ main.cpp text.h classes.h -o dndsh
+echo "Compiling version.cpp..."
 g++ version.cpp -o dndsh-version
+echo "Compiling banner.cpp..."
 g++ banner.cpp -o dndsh-banner
+echo "Compiling roll.cpp..."
+g++ roll.cpp -o dndsh-roll
