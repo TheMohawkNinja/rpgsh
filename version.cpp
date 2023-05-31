99ffc3a4a51eca061d6b21feafb1c74b9584a01c
TheMohawkNinja <themohawkninja@aol.com>
2023-05-30 20:05:21 -0400

    Updated version

--- version.cpp
+++ version.cpp
@@ -3,7 +3,7 @@
 int main()
 {
 	const unsigned char major = 0;
-	const unsigned char minor = 1;
+	const unsigned char minor = 3;
 	const unsigned char patch = 0;
 	fprintf(stdout,"Version %d.%d.%d\n",major,minor,patch);
 
