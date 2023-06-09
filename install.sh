e113294342504670068c698f65ba39da16fb576b
Ben <themohawkninja@aol.com>
2023-06-09 14:51:06 -0400

    Added required compiler flags

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 echo "Compiling main.cpp..."
-g++ main.cpp -o dndsh
+g++ main.cpp text.h classes.h -lstdc++fs -std=c++17 -o dndsh
 echo "Compiling variables.cpp..."
-g++ variables.cpp -o dndsh-variables
+g++ variables.cpp text.h classes.h -lstdc++fs -std=c++17 -o dndsh-variables
 echo "Compiling version.cpp..."
 g++ version.cpp -o dndsh-version
 echo "Compiling banner.cpp..."
 g++ banner.cpp -o dndsh-banner
 echo "Compiling roll.cpp..."
-g++ roll.cpp -lstdc++fs -std=c++17 -o dndsh-roll
+g++ roll.cpp text.h classes.h -lstdc++fs -std=c++17 -o dndsh-roll
