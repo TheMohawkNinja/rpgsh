adbde00727ebbf2f8f08b1e0d835955e9a2b7465
TheMohawkNinja <themohawkninja@aol.com>
2023-06-09 21:59:32 -0400

    Updated to version 0.4.0

--- version.cpp
+++ version.cpp
@@ -3,8 +3,8 @@
 int main()
 {
 	const unsigned char major = 0;
-	const unsigned char minor = 3;
-	const unsigned char patch = 9;
+	const unsigned char minor = 4;
+	const unsigned char patch = 0;
 	fprintf(stdout,"Version %d.%d.%d\n",major,minor,patch);
 
 	return 0;
