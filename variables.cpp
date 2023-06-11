e8faece59f1bd178765a902571ca4a19f4519898
TheMohawkNinja <themohawkninja@aol.com>
2023-06-11 18:28:58 -0400

    Fixed crashing issue with assignment

--- variables.cpp
+++ variables.cpp
@@ -1,20 +1,29 @@
 #include <vector>
 #include "classes.h"
 
+int set_var(std::string var, std::string value)
+{
+	bool old_is_digit, new_is_digit;
+
+	try
+	{
+		std::stoi(std::string(
+	}
+}
+
 int main(int argc, char** argv)
 {
 	DNDSH_CHAR c = DNDSH_CHAR();
 	c.load();
 
+	std::vector<std::string> v_argv = argv_handler(argc, argv);
 	std::vector<std::string> vars;
 
-	fprintf(stdout,"argc=%d\n",argc);
-	for(int i=0; i<argc; i++)
+	for(int i=0; i<v_argv.size(); i++)
 	{
-		fprintf(stdout,"argv[%d]=%s\n",i,argv[i]);
-		if(std::string(argv[i]).substr(0,1) == "%")
+		if(std::string(v_argv[i]).substr(0,1) == "%")
 		{
-			vars.push_back(std::string(argv[i]).substr(1,sizeof(argv[i])-1));
+			vars.push_back(std::string(v_argv[i]).substr(1,sizeof(v_argv[i])-1));
 		}
 	}
 
@@ -24,12 +33,11 @@ int main(int argc, char** argv)
 	}
 	else
 	{
-		fprintf(stdout,"vars[0]=%s\n",vars[0].c_str());
-		if(!strcmp(argv[3],"="))
+		if(v_argv[3] == "=")
 		{
-			fprintf(stdout,"vars[0]=%s\n",vars[0].c_str());
-			c.Attr[vars[0]] = argv[4];
-			fprintf(stdout,"%s\n",std::string(c.Attr[vars[0]]).c_str());
+			std::string old = std::string(c.Attr[vars[0]]);
+			c.Attr[vars[0]] = v_argv[4];
+			DNDSH_OUTPUT(Info,"\"%s\" has been changed from \"%s\" to \"%s\".",vars[0].c_str(),old.c_str(),std::string(c.Attr[vars[0]]).c_str());
 		}
 	}
 
