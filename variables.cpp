8fda4b1aa74370eecee6fcc326f4efee3354501b
TheMohawkNinja <themohawkninja@aol.com>
2023-06-21 19:46:17 -0400

    Partial functionality for arbitrary operators

--- variables.cpp
+++ variables.cpp
@@ -2,7 +2,16 @@
 #include "classes.h"
 
 RPGSH_CHAR c = RPGSH_CHAR();
-
+bool is_operator(std::string s)
+{
+	if(s == "=" ||
+	s == "+=" || s == "-=" || s== "*=" || s == "/=" ||
+	s == "+" || s == "-" || s == "*" || s == "/")
+	{
+		return true;
+	}
+	else{return false;}
+}
 void set_var(std::string var, std::string value)
 {
 	std::string old = std::string(c.Attr[var]);
@@ -51,11 +60,72 @@ int main(int argc, char** argv)
 	{
 		fprintf(stdout,"%s\n",c.Attr[var].c_str());
 	}
+	else if(argc == 4)
+	{
+		if(is_operator(std::string(argv[argc-1])))
+		{
+			RPGSH_OUTPUT(Error,"Expected value after \'%s\'.",argv[argc-1]);
+			exit(-1);
+		}
+		else
+		{
+			RPGSH_OUTPUT(Error,"Expected operator before \'%s\'.",argv[argc-1]);
+			exit(-1);
+		}
+	}
 	else
 	{
+		RPGSH_VAR value = std::string(argv[4]);
+		for(int i=4; i<argc; i++)
+		{
+			if(!i%2 && is_operator(argv[i]))
+			{
+				RPGSH_OUTPUT(Error,"Expected non-operator value at \'%s\'.",argv[i]);
+				exit(-1);
+			}
+			else if(i%2 && !is_operator(argv[i]))
+			{
+				RPGSH_OUTPUT(Error,"Expected operator at \'%s\'.",argv[i]);
+				exit(-1);
+			}
+			else if(i%2)
+			{
+				if(!strcmp(argv[i],"+"))
+				{
+					try{value += std::stoi(argv[i+1]);}
+					catch(...){value += std::string(argv[i+1]);}
+				}
+				else if(!strcmp(argv[i],"-"))
+				{
+					try{value -= std::stoi(argv[i+1]);}
+					catch(...){RPGSH_OUTPUT(Error,"Cannot subtract \'%s\' from left-hand operand.",argv[i+1]);exit(-1);}
+				}
+				else if(!strcmp(argv[i],"*"))
+				{
+					try{value *= std::stoi(argv[i+1]);}
+					catch(...){RPGSH_OUTPUT(Error,"Cannot multiply left-hand operand by \'%s\'.",argv[i+1]);exit(-1);}
+				}
+				else if(!strcmp(argv[i],"/"))
+				{
+					try{value /= std::stoi(argv[i+1]);}
+					catch(...){RPGSH_OUTPUT(Error,"Cannot divide left-hand operand by \'%s\'.",argv[i+1]);exit(-1);}
+				}
+				else
+				{
+					RPGSH_OUTPUT(Error,"Invalid operator \'%s\'.",argv[i]);
+					exit(-1);
+				}
+			}
+		}
 		if(!strcmp(argv[3],"="))
 		{
-			set_var(var,std::string(argv[4]));
+			if(argc == 5)
+			{
+				set_var(var,std::string(argv[4]));
+			}
+			else
+			{
+			}
 		}
 	}
 
