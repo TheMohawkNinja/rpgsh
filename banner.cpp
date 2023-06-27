0fcfe3c1f5407c91475e4e8da8e19cab8b3d837e
TheMohawkNinja <themohawkninja@aol.com>
2023-06-27 19:30:12 -0400

    Minor change to 'R'

--- banner.cpp
+++ banner.cpp
@@ -14,7 +14,7 @@ int main()
 	fprintf(stdout,"%s  │#┌─────┐#││#┌─────┐#││#┌─────┐#│%s┌┘######││#│              %s\n",TEXT_CYAN,TEXT_MAGENTA,TEXT_NORMAL);
 	fprintf(stdout,"%s  │#└─────┘#││#└─────┘#││#│     └─┘%s│#┌─────┘│#│              %s\n",TEXT_CYAN,TEXT_MAGENTA,TEXT_NORMAL);
 	fprintf(stdout,"%s  │########┌┘│########┌┘│#│   ┌───┐%s│#└────┐ │#└────┐         %s\n",TEXT_CYAN,TEXT_MAGENTA,TEXT_NORMAL);
-	fprintf(stdout,"%s  │#┌──┐#─┬┘ │#┌──────┘ │#│   │###│%s└┐#####└┐│######└┐        %s\n",TEXT_CYAN,TEXT_MAGENTA,TEXT_NORMAL);
+	fprintf(stdout,"%s  │#┌──┐##┌┘ │#┌──────┘ │#│   │###│%s└┐#####└┐│######└┐        %s\n",TEXT_CYAN,TEXT_MAGENTA,TEXT_NORMAL);
 	fprintf(stdout,"%s  │#│  └┐#└┐ │#│        │#│   └─┐#│%s └────┐#││#┌───┐#│        %s\n",TEXT_CYAN,TEXT_MAGENTA,TEXT_NORMAL);
 	fprintf(stdout,"%s  │#│   └┐#└┐│#│        │#└─────┘#│%s┌─────┘#││#│   │#│        %s\n",TEXT_CYAN,TEXT_MAGENTA,TEXT_NORMAL);
 	fprintf(stdout,"%s  │#│    └┐#││#│        └┐########│%s│######┌┘│#│   │#│        %s\n",TEXT_CYAN,TEXT_MAGENTA,TEXT_NORMAL);
