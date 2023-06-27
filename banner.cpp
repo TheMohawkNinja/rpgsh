3c7e6784eb5324e5293fd16c738c7ef628ffc182
TheMohawkNinja <themohawkninja@aol.com>
2023-06-27 17:00:14 -0400

    Better monospaced 'R'

--- banner.cpp
+++ banner.cpp
@@ -4,25 +4,25 @@
 
 int main()
 {
-	fprintf(stdout,"                                          %s▟                   %s\n",TEXT_YELLOW,TEXT_NORMAL);
-	fprintf(stdout,"                                          %s█▙                  %s\n",TEXT_YELLOW,TEXT_NORMAL);
-	fprintf(stdout,"  %s▜██████████████████████████████████████%s██████████■██      %s\n",TEXT_LIGHTGRAY,TEXT_YELLOW,TEXT_NORMAL);
-	fprintf(stdout,"                                          %s█▛                  %s\n",TEXT_YELLOW,TEXT_NORMAL);
-	fprintf(stdout,"                                          %s▜                   %s\n",TEXT_YELLOW,TEXT_NORMAL);
-	fprintf(stdout,"  %s┌────────┐  ┌────────┐ ┌────────┐ %s                        %s\n",TEXT_CYAN,TEXT_MAGENTA,TEXT_NORMAL);
-	fprintf(stdout,"  %s│########└┐ │########└┐│########└┐%s ┌──────┐┌─┐            %s\n",TEXT_CYAN,TEXT_MAGENTA,TEXT_NORMAL);
-	fprintf(stdout,"  %s│#┌─────┐#│ │#┌─────┐#││#┌─────┐#│%s┌┘######││#│            %s\n",TEXT_CYAN,TEXT_MAGENTA,TEXT_NORMAL);
-	fprintf(stdout,"  %s│#└─────┘#│ │#└─────┘#││#│     └─┘%s│#┌─────┘│#│            %s\n",TEXT_CYAN,TEXT_MAGENTA,TEXT_NORMAL);
-	fprintf(stdout,"  %s│########┬┘ │########┌┘│#│   ┌───┐%s│#└────┐ │#└────┐       %s\n",TEXT_CYAN,TEXT_MAGENTA,TEXT_NORMAL);
-	fprintf(stdout,"  %s│#┌────┐#└┐ │#┌──────┘ │#│   │###│%s└┐#####└┐│######└┐      %s\n",TEXT_CYAN,TEXT_MAGENTA,TEXT_NORMAL);
-	fprintf(stdout,"  %s│#│    └┐#└┐│#│        │#│   └─┐#│%s └────┐#││#┌───┐#│      %s\n",TEXT_CYAN,TEXT_MAGENTA,TEXT_NORMAL);
-	fprintf(stdout,"  %s│#│     └┐#││#│        │#└─────┘#│%s┌─────┘#││#│   │#│      %s\n",TEXT_CYAN,TEXT_MAGENTA,TEXT_NORMAL);
-	fprintf(stdout,"  %s│#│      │#││#│        └┐########│%s│######┌┘│#│   │#│      %s\n",TEXT_CYAN,TEXT_MAGENTA,TEXT_NORMAL);
-	fprintf(stdout,"  %s└─┘      └─┘└─┘         └────────┘%s└──────┘ └─┘   └─┘      %s\n",TEXT_CYAN,TEXT_MAGENTA,TEXT_NORMAL);
+	fprintf(stdout,"                                          %s▟                    %s\n",TEXT_YELLOW,TEXT_NORMAL);
+	fprintf(stdout,"                                          %s█▙                   %s\n",TEXT_YELLOW,TEXT_NORMAL);
+	fprintf(stdout,"%s  ▜██████████████████████████████████████%s██████████■██       %s\n",TEXT_LIGHTGRAY,TEXT_YELLOW,TEXT_NORMAL);
+	fprintf(stdout,"                                          %s█▛                   %s\n",TEXT_YELLOW,TEXT_NORMAL);
+	fprintf(stdout,"                                          %s▜                    %s\n",TEXT_YELLOW,TEXT_NORMAL);
+	fprintf(stdout,"%s  ┌────────┐ ┌────────┐ ┌────────┐ %s                          %s\n",TEXT_CYAN,TEXT_MAGENTA,TEXT_NORMAL);
+	fprintf(stdout,"%s  │########└┐│########└┐│########└┐%s ┌──────┐┌─┐              %s\n",TEXT_CYAN,TEXT_MAGENTA,TEXT_NORMAL);
+	fprintf(stdout,"%s  │#┌─────┐#││#┌─────┐#││#┌─────┐#│%s┌┘######││#│              %s\n",TEXT_CYAN,TEXT_MAGENTA,TEXT_NORMAL);
+	fprintf(stdout,"%s  │#└─────┘#││#└─────┘#││#│     └─┘%s│#┌─────┘│#│              %s\n",TEXT_CYAN,TEXT_MAGENTA,TEXT_NORMAL);
+	fprintf(stdout,"%s  │########┌┘│########┌┘│#│   ┌───┐%s│#└────┐ │#└────┐         %s\n",TEXT_CYAN,TEXT_MAGENTA,TEXT_NORMAL);
+	fprintf(stdout,"%s  │#┌──┐#─┬┘ │#┌──────┘ │#│   │###│%s└┐#####└┐│######└┐        %s\n",TEXT_CYAN,TEXT_MAGENTA,TEXT_NORMAL);
+	fprintf(stdout,"%s  │#│  └┐#└┐ │#│        │#│   └─┐#│%s └────┐#││#┌───┐#│        %s\n",TEXT_CYAN,TEXT_MAGENTA,TEXT_NORMAL);
+	fprintf(stdout,"%s  │#│   └┐#└┐│#│        │#└─────┘#│%s┌─────┘#││#│   │#│        %s\n",TEXT_CYAN,TEXT_MAGENTA,TEXT_NORMAL);
+	fprintf(stdout,"%s  │#│    └┐#││#│        └┐########│%s│######┌┘│#│   │#│        %s\n",TEXT_CYAN,TEXT_MAGENTA,TEXT_NORMAL);
+	fprintf(stdout,"%s  └─┘     └─┘└─┘         └────────┘%s└──────┘ └─┘   └─┘        %s\n",TEXT_CYAN,TEXT_MAGENTA,TEXT_NORMAL);
 	fprintf(stdout,"%s                                              ▄▄▄▄▄▄▄▄         %s\n",TEXT_RED,TEXT_NORMAL);
-	fprintf(stdout,"%s               ▄                 ▄▄     ▄█   ██%s██████%s██   %s\n",TEXT_RED,TEXT_YELLOW,TEXT_RED,TEXT_NORMAL);
-	fprintf(stdout,"%s  ▀   ▄   ▀       ▀▀  ▄▄   ▀▀        ▄▄     ██%s███%s██%s██%s██  %s\n",TEXT_RED,TEXT_YELLOW,TEXT_WHITE,TEXT_YELLOW,TEXT_RED,TEXT_NORMAL);
-	fprintf(stdout,"%s             ▀                 ▀▀         ▀█ ██%s██████%s██   %s\n",TEXT_RED,TEXT_YELLOW,TEXT_RED,TEXT_NORMAL);
+	fprintf(stdout,"%s               ▄                 ▄▄     ▄█   ██%s██████%s██    %s\n",TEXT_RED,TEXT_YELLOW,TEXT_RED,TEXT_NORMAL);
+	fprintf(stdout,"%s  ▀   ▄   ▀       ▀▀  ▄▄   ▀▀        ▄▄     ██%s███%s██%s██%s██%s\n",TEXT_RED,TEXT_YELLOW,TEXT_WHITE,TEXT_YELLOW,TEXT_RED,TEXT_NORMAL);
+	fprintf(stdout,"%s             ▀                 ▀▀         ▀█ ██%s██████%s██    %s\n",TEXT_RED,TEXT_YELLOW,TEXT_RED,TEXT_NORMAL);
 	fprintf(stdout,"%s                                              ▀▀▀▀▀▀▀▀         %s\n",TEXT_RED,TEXT_NORMAL);
 	fprintf(stdout,"\n");
 	fprintf(stdout,"The interactive Linux shell for table-top RPGs.\n\nBy %s.\n","TheMohawkNinja");
