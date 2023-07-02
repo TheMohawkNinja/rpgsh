8e150145dde481e89a8e816f9261cad4995d8e37
TheMohawkNinja <themohawkninja@aol.com>
2023-07-02 08:33:24 -0400

    +=,-=,*=, and /= operators can now have arbitrarily long RHS

--- variables.cpp
+++ variables.cpp
@@ -27,7 +27,7 @@ bool is_int(char* s)
 		return false;
 	}
 }
-void set_var(std::string var, std::string old, std::string value, bool is_char_var, std::vector<std::string> shell_vars)
+void set_var(std::string var, std::string old_value, std::string new_value, bool is_char_var, std::vector<std::string> shell_vars)
 {
 	std::string var_type;
 
@@ -40,21 +40,21 @@ void set_var(std::string var, std::string old, std::string value, bool is_char_v
 		var_type = "Character attribute";
 	}
 
-	bool old_is_digit, new_is_digit;
+	bool old_value_is_digit, new_is_digit;
 
 	try
 	{
-		std::stoi(old);
-		old_is_digit = true;
+		std::stoi(old_value);
+		old_value_is_digit = true;
 	}
 	catch(...)
 	{
-		old_is_digit = false;
+		old_value_is_digit = false;
 	}
 
 	try
 	{
-		std::stoi(value);
+		std::stoi(new_value);
 		new_is_digit = true;
 	}
 	catch(...)
@@ -62,11 +62,11 @@ void set_var(std::string var, std::string old, std::string value, bool is_char_v
 		new_is_digit = false;
 	}
 
-	if(old_is_digit && !new_is_digit)
+	if(old_value_is_digit && !new_is_digit)
 	{
 		RPGSH_OUTPUT(Warning,"%s \"%s\" is changing from an integer to a string.",var_type.c_str(),var.c_str());
 	}
-	else if(!old_is_digit && new_is_digit)
+	else if(!old_value_is_digit && new_is_digit)
 	{
 		RPGSH_OUTPUT(Warning,"%s \"%s\" is changing from a string to an integer.",var_type.c_str(),var.c_str());
 	}
@@ -79,15 +79,16 @@ void set_var(std::string var, std::string old, std::string value, bool is_char_v
 		{
 			ofs<<shell_vars[i]+"\n";
 		}
-		ofs<<var+v.DataSeparator+value;
+		ofs<<var+v.DataSeparator+new_value;
 		ofs.close();
 	}
 	else
 	{
-		c.Attr[var] = value;
+		c.Attr[var] = new_value;
+		c.set_current();
 		c.save();
 	}
-	RPGSH_OUTPUT(Info,"%s \"%s\" has been changed from \"%s\" to \"%s\".",var_type.c_str(),var.c_str(),old.c_str(),value.c_str());
+	RPGSH_OUTPUT(Info,"%s \"%s\" has been changed from \"%s\" to \"%s\".",var_type.c_str(),var.c_str(),old_value.c_str(),new_value.c_str());
 }
 
 int main(int argc, char** argv)
@@ -95,7 +96,7 @@ int main(int argc, char** argv)
 	bool is_char_var = (argv[2][0] == CHAR_VAR);
 	std::vector<std::string> shell_vars;
 	std::string var = std::string(argv[2]).substr(1,std::string(argv[2]).length()-1);
-	std::string old;
+	RPGSH_VAR old_value;
 
 	if(!is_char_var)
 	{
@@ -107,7 +108,7 @@ int main(int argc, char** argv)
 
 			if(data.substr(0,data.find(v.DataSeparator)) == var)
 			{
-				old = data.substr(data.find(v.DataSeparator)+v.DataSeparator.length(),
+				old_value = data.substr(data.find(v.DataSeparator)+v.DataSeparator.length(),
 						data.length()-(data.find(v.DataSeparator)+v.DataSeparator.length()));
 			}
 			else
@@ -120,18 +121,18 @@ int main(int argc, char** argv)
 	else
 	{
 		c.load(false);
-		old = std::string(c.Attr[var]);
+		old_value = std::string(c.Attr[var]);
 	}
 
 	if(argc == 3)
 	{
-		fprintf(stdout,"%s\n",old.c_str());
+		fprintf(stdout,"%s\n",old_value.c_str());
 	}
 	else if(argc == 4)
 	{
 		if(is_operator(std::string(argv[3])))
 		{
-			RPGSH_OUTPUT(Error,"Expected value after \'%s\'.",argv[argc-1]);
+			RPGSH_OUTPUT(Error,"Expected new_value after \'%s\'.",argv[argc-1]);
 			exit(-1);
 		}
 		else
@@ -140,82 +141,14 @@ int main(int argc, char** argv)
 			exit(-1);
 		}
 	}
-	else if(argc == 5)
-	{
-		if(!is_operator(argv[3]))
-		{
-			RPGSH_OUTPUT(Error,"Expected operator at \'%s\'.",argv[3]);
-			exit(-1);
-		}
-
-		RPGSH_VAR value = RPGSH_VAR(old);
-		if(!strcmp(argv[3],"="))
-		{
-			set_var(var,old,argv[4],is_char_var,shell_vars);
-			return 0;
-		}
-		else if(!strcmp(argv[3],"+="))
-		{
-			if(is_int(argv[4]))
-			{
-				value += std::stoi(argv[4]);
-			}
-			else
-			{
-				value += std::string(argv[4]);
-			}
-		}
-		else if(!strcmp(argv[3],"-="))
-		{
-			if(is_int(argv[4]))
-			{
-				value -= std::stoi(argv[4]);
-			}
-			else
-			{
-				RPGSH_OUTPUT(Error,"Cannot subtract \'%s\' from left-hand operand.",argv[4]);
-				exit(-1);
-			}
-		}
-		else if(!strcmp(argv[3],"*="))
-		{
-			if(is_int(argv[4]))
-			{
-				value *= std::stoi(argv[4]);
-			}
-			else
-			{
-				RPGSH_OUTPUT(Error,"Cannot multiply left-hand operand by \'%s\'.",argv[4]);
-				exit(-1);
-			}
-		}
-		else if(!strcmp(argv[3],"/="))
-		{
-			if(is_int(argv[4]))
-			{
-				value /= std::stoi(argv[4]);
-			}
-			else
-			{
-				RPGSH_OUTPUT(Error,"Cannot divide left-hand operand by \'%s\'.",argv[4]);
-				exit(-1);
-			}
-		}
-		else
-		{
-			RPGSH_OUTPUT(Error,"Invalid operator \'%s\'.",argv[3]);
-			exit(-1);
-		}
-		set_var(var,old,std::string(value),is_char_var,shell_vars);
-	}
 	else
 	{
-		RPGSH_VAR value = std::string(argv[4]);
+		RPGSH_VAR new_value = std::string(argv[4]);
 		for(int i=4; i<argc; i++)
 		{
 			if(!i%2 && is_operator(argv[i]))
 			{
-				RPGSH_OUTPUT(Error,"Expected non-operator value at \'%s\'.",argv[i]);
+				RPGSH_OUTPUT(Error,"Expected non-operator new_value at \'%s\'.",argv[i]);
 				exit(-1);
 			}
 			else if(i%2 && !is_operator(argv[i]))
@@ -225,52 +158,22 @@ int main(int argc, char** argv)
 			}
 			else if(i%2)
 			{
+				RPGSH_VAR current_operand = std::string(argv[i+1]);
 				if(!strcmp(argv[i],"+"))
 				{
-					if(is_int(argv[i+1]))
-					{
-						value += std::stoi(argv[i+1]);
-					}
-					else
-					{
-						value += std::string(argv[i+1]);
-					}
+					new_value += current_operand;
 				}
 				else if(!strcmp(argv[i],"-"))
 				{
-					if(is_int(argv[i+1]))
-					{
-						value -= std::stoi(argv[i+1]);
-					}
-					else
-					{
-						RPGSH_OUTPUT(Error,"Cannot subtract \'%s\' from left-hand operand.",argv[i+1]);
-						exit(-1);
-					}
+					new_value -= current_operand;
 				}
 				else if(!strcmp(argv[i],"*"))
 				{
-					if(is_int(argv[i+1]))
-					{
-						value *= std::stoi(argv[i+1]);
-					}
-					else
-					{
-						RPGSH_OUTPUT(Error,"Cannot multiply left-hand operand by \'%s\'.",argv[i+1]);
-						exit(-1);
-					}
+					new_value *= current_operand;
 				}
 				else if(!strcmp(argv[i],"/"))
 				{
-					if(is_int(argv[i+1]))
-					{
-						value /= std::stoi(argv[i+1]);
-					}
-					else
-					{
-						RPGSH_OUTPUT(Error,"Cannot divide left-hand operand by \'%s\'.",argv[i+1]);
-						exit(-1);
-					}
+					new_value /= current_operand;
 				}
 				else
 				{
@@ -279,13 +182,37 @@ int main(int argc, char** argv)
 				}
 			}
 		}
-		set_var(var,old,std::string(value),is_char_var,shell_vars);
-	}
 
-	if(is_char_var)
-	{
-		c.set_current();
-		c.save();
+		if(!strcmp(argv[3],"="))
+		{
+			set_var(var,std::string(old_value),std::string(new_value),is_char_var,shell_vars);
+			return 0;
+		}
+		else if(!strcmp(argv[3],"+="))
+		{
+			set_var(var,std::string(old_value),std::string(old_value+new_value),is_char_var,shell_vars);
+			return 0;
+		}
+		else if(!strcmp(argv[3],"-="))
+		{
+			set_var(var,std::string(old_value),std::string(old_value-new_value),is_char_var,shell_vars);
+			return 0;
+		}
+		else if(!strcmp(argv[3],"*="))
+		{
+			set_var(var,std::string(old_value),std::string(old_value*new_value),is_char_var,shell_vars);
+			return 0;
+		}
+		else if(!strcmp(argv[3],"/="))
+		{
+			set_var(var,std::string(old_value),std::string(old_value/new_value),is_char_var,shell_vars);
+			return 0;
+		}
+		else
+		{
+			RPGSH_OUTPUT(Error,"Invalid operator \'%s\'.",argv[3]);
+			exit(-1);
+		}
 	}
 	return 0;
 }
