76b619f78667b836c512f5951d358aee6d6a8de1
Ben <themohawkninja@aol.com>
2023-08-04 11:12:30 -0400

    Implemented macros

--- version.cpp
+++ version.cpp
@@ -1,11 +1,9 @@
 #include <stdio.h>
+#include "classes.h"
 
 int main()
 {
-	const unsigned char major = 0;
-	const unsigned char minor = 5;
-	const unsigned char patch = 7;
-	fprintf(stdout,"Version %d.%d.%d\n",major,minor,patch);
+	fprintf(stdout,"Version %d.%d.%d\n",MAJOR_VERSION,MINOR_VERSION,PATCH_VERSION);
 
 	return 0;
 }
