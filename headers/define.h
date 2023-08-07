63ba8506f918ee29ddd88ba9dd65156b70dcfe76
TheMohawkNinja <themohawkninja@aol.com>
2023-08-06 21:33:09 -0400

    Updated to 0.6.0

--- headers/define.h
+++ headers/define.h
@@ -5,8 +5,8 @@
 
 //Version info
 #define MAJOR_VERSION		0
-#define MINOR_VERSION		5
-#define PATCH_VERSION		8
+#define MINOR_VERSION		6
+#define PATCH_VERSION		0
 
 //Comment character definition
 #define COMMENT			'#'
