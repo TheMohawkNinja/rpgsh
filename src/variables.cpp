6576b8451f84d6b43a6ddd53a10c455efcadcac8
Ben <themohawkninja@aol.com>
2024-03-05 11:15:31 -0500

    Updated sigil names

--- src/variables.cpp
+++ src/variables.cpp
@@ -6,14 +6,10 @@ RPGSH_CHAR c = RPGSH_CHAR();
 
 bool is_operator(std::string s)
 {
-	if(s == "="  ||
-	   s == "+"  || s == "-"  || s == "*"  || s == "/"  ||
-	   s == "+=" || s == "-=" || s == "*=" || s == "/=" ||
-	   s == "++" || s == "--")
-	{
-		return true;
-	}
-	else{return false;}
+	return (s == "="  ||
+	   	s == "+"  || s == "-"  || s == "*"  || s == "/"  ||
+	  	s == "+=" || s == "-=" || s == "*=" || s == "/=" ||
+	  	s == "++" || s == "--");
 }
 bool is_int(std::string s)
 {
@@ -27,65 +23,79 @@ bool is_int(std::string s)
 		return false;
 	}
 }
-void set_var(std::string var, RPGSH_VAR old_value, RPGSH_VAR new_value, bool is_char_var, std::vector<std::string> shell_vars)
+void set_var(std::string var, RPGSH_VAR old_value, RPGSH_VAR new_value, char scope_sigil, std::vector<std::string> shell_vars)
 {
 	std::string var_type;
 
-	if(!is_char_var)
+	switch(scope_sigil)
 	{
-		var_type = "Shell variable";
-	}
-	else
-	{
-		var_type = "Character attribute";
+		case CHAR_SIGIL:
+			var_type = "Character attribute";
+			break;
+		case CAMPAIGN_SIGIL:
+			var_type = "Campaign variable";
+			break;
+		case SHELL_SIGIL:
+			var_type = "Shell variable";
+			break;
+		default:
+			output(Error,"Unknown scope sigil \'%c\'.",scope_sigil);
+			exit(-1);
 	}
 
-	bool old_is_digit = is_int(old_value.Value);
-	bool new_is_digit = is_int(new_value.Value);
+	bool old_is_int = is_int(old_value.Value);
+	bool new_is_int = is_int(new_value.Value);
 
-	if(old_is_digit && !new_is_digit)
-	{
+	if(old_is_int && !new_is_int)
 		output(Warning,"%s \"%s\" is changing from an integer to a string.",var_type.c_str(),var.c_str());
-	}
-	else if(!old_is_digit && new_is_digit)
-	{
+	else if(!old_is_int && new_is_int)
 		output(Warning,"%s \"%s\" is changing from a string to an integer.",var_type.c_str(),var.c_str());
-	}
 
-	if(!is_char_var)
+	switch(scope_sigil)
 	{
-		set_shell_var(var,std::string(new_value));
+		case CHAR_SIGIL:
+			c.Attr[var] = new_value;
+			c.save();
+			c.set_as_current();
+			break;
+		case CAMPAIGN_SIGIL:
+			//TODO
+			break;
+		case SHELL_SIGIL:
+			set_shell_var(var,std::string(new_value));
+			break;
 	}
-	else
-	{
-		c.Attr[var] = new_value;
-		c.save();
-		c.set_as_current();
-	}
-	output(Info,"%s \"%s\" has been changed from \"%s\" to \"%s\".",var_type.c_str(),var.c_str(),old_value.c_str(),new_value.c_str());
+	if(old_value != new_value)
+		output(Info,"%s \"%s\" has been changed from \"%s\" to \"%s\".",var_type.c_str(),var.c_str(),old_value.c_str(),new_value.c_str());
 }
 
 int main(int argc, char** argv)
 {
-	if(std::string(argv[1]).length() == 1)// If all the user enters is '$' or '%')
+	if(std::string(argv[1]).length() == 1)// If the user only enters the scope sigil
 	{
 		output(Error,"Empty variable name.");
 		exit(-1);
 	}
 
-	bool is_char_var = (argv[1][0] == CHAR_VAR);
 	std::vector<std::string> shell_vars;
 	std::string var = std::string(argv[1]).substr(1,std::string(argv[1]).length()-1);
 	RPGSH_VAR old_value;
 
-	if(!is_char_var)
+	switch(argv[1][0])
 	{
-		old_value = get_shell_var(var);
-	}
-	else
-	{
-		c.load(get_shell_var(CURRENT_CHAR_SHELL_VAR),false);
-		old_value = std::string(c.Attr[var]);
+		case CHAR_SIGIL:
+			c.load(get_shell_var(CURRENT_CHAR_SHELL_VAR),false);
+			old_value = std::string(c.Attr[var]);
+			break;
+		case CAMPAIGN_SIGIL:
+			// TODO
+			break;
+		case SHELL_SIGIL:
+			old_value = get_shell_var(var);
+			break;
+		default:
+			output(Error,"Unknown scope sigil \'%c\'.",argv[1][0]);
+			exit(-1);
 	}
 
 	if(argc == 2)
@@ -98,12 +108,12 @@ int main(int argc, char** argv)
 		{
 			if(std::string(argv[2]) == "++")
 			{
-				set_var(var,old_value,old_value+1,is_char_var,shell_vars);
+				set_var(var,old_value,old_value+1,argv[1][0],shell_vars);
 				return 0;
 			}
 			else if(std::string(argv[2]) == "--")
 			{
-				set_var(var,old_value,old_value-1,is_char_var,shell_vars);
+				set_var(var,old_value,old_value-1,argv[1][0],shell_vars);
 				return 0;
 			}
 			else
@@ -162,27 +172,27 @@ int main(int argc, char** argv)
 
 		if(!strcmp(argv[2],"="))
 		{
-			set_var(var,old_value,new_value,is_char_var,shell_vars);
+			set_var(var,old_value,new_value,argv[1][0],shell_vars);
 			return 0;
 		}
 		else if(!strcmp(argv[2],"+="))
 		{
-			set_var(var,old_value,std::string(old_value+new_value),is_char_var,shell_vars);
+			set_var(var,old_value,std::string(old_value+new_value),argv[1][0],shell_vars);
 			return 0;
 		}
 		else if(!strcmp(argv[2],"-="))
 		{
-			set_var(var,old_value,std::string(old_value-new_value),is_char_var,shell_vars);
+			set_var(var,old_value,std::string(old_value-new_value),argv[1][0],shell_vars);
 			return 0;
 		}
 		else if(!strcmp(argv[2],"*="))
 		{
-			set_var(var,old_value,std::string(old_value*new_value),is_char_var,shell_vars);
+			set_var(var,old_value,std::string(old_value*new_value),argv[1][0],shell_vars);
 			return 0;
 		}
 		else if(!strcmp(argv[2],"/="))
 		{
-			set_var(var,old_value,std::string(old_value/new_value),is_char_var,shell_vars);
+			set_var(var,old_value,std::string(old_value/new_value),argv[1][0],shell_vars);
 			return 0;
 		}
 		else
