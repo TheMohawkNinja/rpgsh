7c6d78a5e764785f47a8f64cf19d7eaeb3c33665
TheMohawkNinja <themohawkninja@aol.com>
2024-03-06 19:35:42 -0500

    Updated version to 0.6.2 and removed unneccesary #define

--- headers/define.h
+++ headers/define.h
@@ -4,25 +4,26 @@
 #include <unistd.h>//getlogin()
 
 //Version info
-#define MAJOR_VERSION		0
-#define MINOR_VERSION		6
-#define PATCH_VERSION		1
+#define MAJOR_VERSION	0
+#define MINOR_VERSION	6
+#define PATCH_VERSION	2
 
 //Comment character definition
-#define COMMENT			'#'
+#define COMMENT		'#'
 
 //Variable character scope sigils
-#define SHELL_SIGIL		'$'
-#define CAMPAIGN_SIGIL		'#'
-#define CHAR_SIGIL		'@'
+#define SHELL_SIGIL	'$'
+#define CAMPAIGN_SIGIL	'#'
+#define CHAR_SIGIL	'@'
 
 //Built-in variables
-#define CURRENT_CHAR_SHELL_VAR	".CURRENT_CHAR"
-#define CHAR_NAME_ATTR		".NAME"
+#define CURRENT_CHAR_SHELL_VAR		".CURRENT_CHAR"
+#define CURRENT_CAMPAIGN_SHELL_VAR	".CURRENT_CAMPAIGN"
+#define CHAR_NAME_ATTR			".NAME"
 
 //Setting names
-#define PADDING			"padding"
-#define DEFAULT_GAME		"default_game"
+#define PADDING		"padding"
+#define DEFAULT_GAME	"default_game"
 
 //Locations of files to get random data from for things like 'roll'
 const char* random_seed_path = "/dev/random";
@@ -31,13 +32,11 @@ const char* backup_random_seed_path = "/dev/urandom";
 //Internal file/directory paths
 std::string user = getlogin();
 std::string root_dir = "/home/"+user+"/.rpgsh/";
-std::string shell_vars_file = root_dir+".shell";
+std::string shell_vars_file = root_dir+".vars";
 std::string config_file = root_dir+"config";
-std::string characters_dir = root_dir + "characters/";
+std::string campaigns_dir = root_dir + "campaigns/";
 std::string templates_dir = root_dir + "templates/";
 std::string dice_lists_dir = root_dir + "dice-lists/";
 
 //Data separator string. Name shortened for brevity in the code.
 std::string DS = "::";
-
-
