9c9b408555c5e6f2460373ff4a77d8218b03d6a8
TheMohawkNinja <themohawkninja@aol.com>
2024-07-20 09:07:55 -0400

    Resolved pedantic warning

--- src/version.cpp
+++ src/version.cpp
@@ -5,6 +5,9 @@
 
 int main(int argc, char** argv)
 {
+	if(argc > 2)
+		output(Warning,"version only expects 0 or 1 arguments, ignoring all other arguments.");
+
 	check_print_app_description(argv,"Prints version information.");
 
 	fprintf(stdout,"Version %d.%d.%d\n",MAJOR_VERSION,MINOR_VERSION,PATCH_VERSION);
