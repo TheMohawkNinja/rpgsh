31c43507528c33983cec93443cbb6ba61014cb66
TheMohawkNinja <themohawkninja@aol.com>
2024-07-20 09:04:00 -0400

    Resolved pedantic warnings

--- src/help.cpp
+++ src/help.cpp
@@ -4,6 +4,9 @@
 
 int main(int argc, char** argv)
 {
+	if(argc > 2)
+		output(Warning,"help only expects 0 or 1 arguments, ignoring all other arguments.");
+
 	check_print_app_description(argv,"Prints this helpful list of applications and what they do.");
 
 	//Generate list of apps from cache file
@@ -18,10 +21,10 @@ int main(int argc, char** argv)
 	ifs.close();
 
 	//Determine longest name for column formatting
-	int longestNameLength = 0;
+	long unsigned int longestNameLength = 0;
 	std::string appname;
 	std::vector<std::string> appdescription;
-	for(int i=0; i<applications.size(); i++)
+	for(long unsigned int i=0; i<applications.size(); i++)
 	{
 		std::string app = applications[i];
 		if(app.substr(0,prefix.length()) == prefix)
