9d71226fd660872709a86b1af3c8fe988dcaec7a
TheMohawkNinja <themohawkninja@aol.com>
2024-11-20 18:11:14 -0500

    Moved appendMap() appendOutput(), and getSet() to here

--- src/functions.cpp
+++ src/functions.cpp
@@ -388,6 +388,76 @@ unsigned int getWalletValue(Wallet w)
 	return total;
 }
 
+template<typename T>
+void appendMap(Scope scope, std::map<std::string,std::string>* p_map)
+{
+	for(const auto& [k,v] : scope.getDatamap<T>())
+		(*p_map)[k] = scope.getStr<T>(k);
+}
+void appendOutput(std::map<std::string,std::string> map, std::string key, std::string* pOutput)
+{
+	for(const auto& [k,v] : map)
+	{
+		// Get root variable if it exists
+		if(!stringcasecmp((left(k,key.length())+"/"),key))
+			pOutput->append(k + DS + v + DS);
+
+		// Get all subkeys
+		if(!stringcasecmp((left(k,key.length())),key))
+			pOutput->append(k + DS + v + DS);
+	}
+}
+
+std::string getSet(VariableInfo vi)
+{
+	std::map<std::string,std::string> c_map;
+	std::map<std::string,std::string> cs_map;
+	std::map<std::string,std::string> d_map;
+	std::map<std::string,std::string> v_map;
+	std::map<std::string,std::string> w_map;
+
+	// When printing entire containers, treat type sigil as a filter
+	switch(vi.variable[1])
+	{
+		case CURRENCY_SIGIL:
+			appendMap<Currency>(vi.scope,&c_map);
+			break;
+		case CURRENCYSYSTEM_SIGIL:
+			appendMap<CurrencySystem>(vi.scope,&cs_map);
+			break;
+		case DICE_SIGIL:
+			appendMap<Dice>(vi.scope,&d_map);
+			break;
+		case VAR_SIGIL:
+			appendMap<Var>(vi.scope,&v_map);
+			break;
+		case WALLET_SIGIL:
+			appendMap<Wallet>(vi.scope,&w_map);
+			break;
+		case '/':
+			appendMap<Currency>(vi.scope,&c_map);
+			appendMap<CurrencySystem>(vi.scope,&cs_map);
+			appendMap<Dice>(vi.scope,&d_map);
+			appendMap<Var>(vi.scope,&v_map);
+			appendMap<Wallet>(vi.scope,&w_map);
+			break;
+		default:
+			output(Error,"Unknown type specifier \'%c\' in \"%s\"",vi.variable[1],vi.variable.c_str());
+			exit(-1);
+	}
+
+	// Create output string from map
+	std::string output = "";
+	appendOutput(c_map,vi.key,&output);
+	appendOutput(cs_map,vi.key,&output);
+	appendOutput(d_map,vi.key,&output);
+	appendOutput(v_map,vi.key,&output);
+	appendOutput(w_map,vi.key,&output);
+
+	// Cut off the extraneous DS
+	return left(output,output.length()-DS.length());
+}
+
 template<>//Forward definition for <Dice,Dice> due to it being called before being defined.
 bool approxEquals<Dice,Dice>(Dice lhs, Dice rhs);
 template<>
