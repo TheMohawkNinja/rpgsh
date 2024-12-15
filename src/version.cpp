9aafc73af87ecb22274055145990bb494c529f5b
TheMohawkNinja <themohawkninja@aol.com>
2024-12-15 18:02:04 -0500

    Simplified code

--- src/version.cpp
+++ src/version.cpp
@@ -10,27 +10,7 @@ int main(int argc, char** argv)
 
 	check_print_app_description(argv,"Prints version information.");
 
-	if(argv[1] && (!strcmp(argv[1],"-M") || !strcmp(argv[1],"--major")))
-	{
-		fprintf(stdout,"%d\n",MAJOR_VERSION);
-		return 0;
-	}
-	else if(argv[1] && (!strcmp(argv[1],"-m") || !strcmp(argv[1],"--minor")))
-	{
-		fprintf(stdout,"%d\n",MINOR_VERSION);
-		return 0;
-	}
-	else if(argv[1] && (!strcasecmp(argv[1],"-p") || !strcasecmp(argv[1],"--patch")))
-	{
-		fprintf(stdout,"%d\n",PATCH_VERSION);
-		return 0;
-	}
-	else if(argv[1] && (!strcasecmp(argv[1],"-v") || !strcasecmp(argv[1],"--version")))
-	{
-		fprintf(stdout,"%d.%d.%d\n",MAJOR_VERSION,MINOR_VERSION,PATCH_VERSION);
-		return 0;
-	}
-	else if(argv[1] && (!strcasecmp(argv[1],"-?") || !strcasecmp(argv[1],"--help")))
+	if(argv[1] && (!strcasecmp(argv[1],"-?") || !strcasecmp(argv[1],"--help")))
 	{
 		fprintf(stdout,"Prints version information.\n");
 		fprintf(stdout,"USAGE:\n");
@@ -43,6 +23,16 @@ int main(int argc, char** argv)
 		return 0;
 	}
 
-	fprintf(stdout,"Version %d.%d.%d\n",MAJOR_VERSION,MINOR_VERSION,PATCH_VERSION);
+	if(argv[1] && (!strcmp(argv[1],"-M") || !strcmp(argv[1],"--major")))
+		fprintf(stdout,"%d\n",MAJOR_VERSION);
+	else if(argv[1] && (!strcmp(argv[1],"-m") || !strcmp(argv[1],"--minor")))
+		fprintf(stdout,"%d\n",MINOR_VERSION);
+	else if(argv[1] && (!strcasecmp(argv[1],"-p") || !strcasecmp(argv[1],"--patch")))
+		fprintf(stdout,"%d\n",PATCH_VERSION);
+	else if(argv[1] && (!strcasecmp(argv[1],"-v") || !strcasecmp(argv[1],"--version")))
+		fprintf(stdout,"%d.%d.%d\n",MAJOR_VERSION,MINOR_VERSION,PATCH_VERSION);
+	else
+		fprintf(stdout,"Version %d.%d.%d\n",MAJOR_VERSION,MINOR_VERSION,PATCH_VERSION);
+
 	return 0;
 }
