a2192040337d0e4ba5384fbce7f1c12baa3b2436
TheMohawkNinja <themohawkninja@aol.com>
2024-12-17 19:17:40 -0500

    Updated version to 0.11.0

--- headers/define.h
+++ headers/define.h
@@ -5,8 +5,8 @@
 
 //Version info
 #define MAJOR_VERSION			0
-#define MINOR_VERSION			10
-#define PATCH_VERSION			2
+#define MINOR_VERSION			11
+#define PATCH_VERSION			0
 
 //Comment character definition
 #define COMMENT				'!'
