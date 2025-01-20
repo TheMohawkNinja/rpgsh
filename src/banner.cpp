fba193c7c458ed466811848d81c2fa8998c2c8cf
TheMohawkNinja <themohawkninja@aol.com>
2025-01-20 18:02:57 -0500

    Rewrote with options and help text

--- src/banner.cpp
+++ src/banner.cpp
@@ -5,18 +5,16 @@
 #include "../headers/functions.h"
 #include "../headers/text.h"
 
-int main(int argc, char** argv)
+void printSword()
 {
-	if(argc > 2)
-		output(Warning,"banner only expects 0 or 1 arguments, ignoring all other arguments.");
-
-	chkPrntAppDesc(argv,"Prints pretty banner art and the author's signature.");
-
 	fprintf(stdout,"                                         %s▟                   %s\n",TEXT_YELLOW,TEXT_NORMAL);
 	fprintf(stdout,"                                         %s█▙        ▁▁        %s\n",TEXT_YELLOW,TEXT_NORMAL);
-	fprintf(stdout,"%s  ▜█████████████████████████████████████%s█%s███▒▒▒▒▒▒███%s     %s\n",TEXT_LIGHTGRAY,TEXT_YELLOW,TEXT_BG_BLACK,TEXT_BG_DEFAULTCOLOR,TEXT_NORMAL);
+	fprintf(stdout,"%s  ▜█████████████████████████████████████%s█%s███▒▒▒▒▒▒███%s  %s\n",TEXT_LIGHTGRAY,TEXT_YELLOW,TEXT_BG_BLACK,TEXT_BG_DEFAULTCOLOR,TEXT_NORMAL);
 	fprintf(stdout,"                                         %s█▛        ▔▔        %s\n",TEXT_YELLOW,TEXT_NORMAL);
 	fprintf(stdout,"                                         %s▜                   %s\n",TEXT_YELLOW,TEXT_NORMAL);
+}
+void printName()
+{
 	fprintf(stdout,"%s  ┌────────┐ ┌────────┐ ┌────────┐ %s                        %s\n",TEXT_CYAN,TEXT_MAGENTA,TEXT_NORMAL);
 	fprintf(stdout,"%s  │########└┐│########└┐│########└┐%s ┌──────┐┌─┐            %s\n",TEXT_CYAN,TEXT_MAGENTA,TEXT_NORMAL);
 	fprintf(stdout,"%s  │#┌─────┐#││#┌─────┐#││#┌─────┐#│%s┌┘$$$$$$││$│            %s\n",TEXT_CYAN,TEXT_MAGENTA,TEXT_NORMAL);
@@ -27,12 +25,56 @@ int main(int argc, char** argv)
 	fprintf(stdout,"%s  │#│   └┐#└┐│#│        │#└─────┘#│%s┌─────┘$││$│   │$│      %s\n",TEXT_CYAN,TEXT_MAGENTA,TEXT_NORMAL);
 	fprintf(stdout,"%s  │#│    └┐#││#│        └┐########│%s│$$$$$$┌┘│$│   │$│      %s\n",TEXT_CYAN,TEXT_MAGENTA,TEXT_NORMAL);
 	fprintf(stdout,"%s  └─┘     └─┘└─┘         └────────┘%s└──────┘ └─┘   └─┘      %s\n",TEXT_CYAN,TEXT_MAGENTA,TEXT_NORMAL);
+}
+void printFireball()
+{
 	fprintf(stdout,"%s                                            ▄▄▄▄▄▄▄▄         %s\n",TEXT_RED,TEXT_NORMAL);
 	fprintf(stdout,"%s                               ▄▄     ▄█   ██%s██████%s██    %s\n",TEXT_RED,TEXT_YELLOW,TEXT_RED,TEXT_NORMAL);
 	fprintf(stdout,"%s  ▀   ▄   ▀     ▀▀  ▄▄   ▀▀        ▄▄     ██%s███%s██%s██%s██%s\n",TEXT_RED,TEXT_YELLOW,TEXT_WHITE,TEXT_YELLOW,TEXT_RED,TEXT_NORMAL);
 	fprintf(stdout,"%s             ▀               ▀▀         ▀█ ██%s██████%s██    %s\n",TEXT_RED,TEXT_YELLOW,TEXT_RED,TEXT_NORMAL);
 	fprintf(stdout,"%s                                            ▀▀▀▀▀▀▀▀         %s\n",TEXT_RED,TEXT_NORMAL);
-	fprintf(stdout,"\n");
+}
+void printSignature()
+{
 	fprintf(stdout,"The interactive Linux shell for table-top RPGs.\n\nBy %s%s%s%s\n",TEXT_ITALIC,TEXT_LIGHTBLUE,"bd@0xbd.net",TEXT_NORMAL);
+}
+int main(int argc, char** argv)
+{
+	chkPrntAppDesc(argv,"Prints pretty banner art and the author's signature.");
+
+	bool sword, name, fireball, signature;
+	sword = name = fireball = signature = false;
+
+	if(isRequestingHelp(argv))
+	{
+		fprintf(stdout,"USAGE:\n");
+		fprintf(stdout,"\tbanner [%sOPTIONS%s]\n",TEXT_ITALIC,TEXT_NORMAL);
+		fprintf(stdout,"\nOPTIONS:\n");
+		fprintf(stdout,"\t%snone%s\t\tPrints the sword, name, fireball, and signature.\n",TEXT_ITALIC,TEXT_NORMAL);
+		fprintf(stdout,"\t--sword\t\tPrints the sword.\n");
+		fprintf(stdout,"\t--name\t\tPrints the title.\n");
+		fprintf(stdout,"\t--fireball\tPrints the fireball.\n");
+		fprintf(stdout,"\t--signature\tPrints the signature.\n");
+		fprintf(stdout,"\t%s | %s\tPrints this help text.\n",FLAG_HELPSHORT,FLAG_HELPLONG);
+
+		return 0;
+	}
+
+	for(int i=1; i<argc; i++)
+	{
+		if(!strcmp(argv[i],"--sword")) sword = true;
+		else if(!strcmp(argv[i],"--name")) name = true;
+		else if(!strcmp(argv[i],"--fireball")) fireball = true;
+		else if(!strcmp(argv[i],"--signature")) signature = true;
+	}
+
+	if(argc == 1) sword = name = fireball = signature = true;
+
+	if(sword) printSword();
+	if(name) printName();
+	if(fireball) printFireball();
+	if(signature && (sword | name | fireball)) fprintf(stdout,"\n");
+	if(signature) printSignature();
+
 	return 0;
 }
