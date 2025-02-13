826ab98557ae3dd7bbdc3e4265719917ddff7c91
TheMohawkNinja <themohawkninja@aol.com>
2025-02-12 19:27:46 -0500

    Fixed output formatting

--- src/eval.cpp
+++ src/eval.cpp
@@ -191,10 +191,11 @@ std::string doAction(VariableInfo* p_vi, Action action, std::string value)
 					break;
 			}
 
+			std::string xref = (p_vi->xref != "" ? "["+p_vi->xref+"]" : "");
 			if(action == SetAddA)
-				output(Info,"Set \"%c[%s]%c/%s\" to \"%s\"",p_vi->scope.sigil,p_vi->xref.c_str(),p_vi->evalType,(p_vi->key+k).c_str(),v.c_str());
+				output(Info,"Set \"%c%s%c/%s\" to \"%s\"",p_vi->scope.sigil,xref.c_str(),p_vi->evalType,(p_vi->key+k).c_str(),v.c_str());
 			else if(action == SetRemoveA && rk.isRemoved)
-				output(Warning,"Removed \"%c[%s]%c/%s\"",p_vi->scope.sigil,p_vi->xref.c_str(),rk.type,k.c_str());
+				output(Warning,"Removed \"%c%s%c/%s\"",p_vi->scope.sigil,xref.c_str(),rk.type,k.c_str());
 		}
 
 		if (action == SetAdd || action == SetRemove) return getSetStr(*p_vi);
@@ -711,15 +712,16 @@ int main(int argc, char** argv)
 		}
 
 		std::string new_value = getAppOutput(std::string(argv[1])).output[0];
+		std::string xref = (vi.xref != "" ? "["+vi.xref+"]" : "");
 		if(old_value == "")
-			output(Info,"%c[%s]%c/%s has been initialized to %s",
-			       vi.scope.sigil,vi.xref.c_str(),vi.evalType,vi.key.c_str(),new_value.c_str());
+			output(Info,"%c%s%c/%s has been initialized to %s",
+			       vi.scope.sigil,xref.c_str(),vi.evalType,vi.key.c_str(),new_value.c_str());
 		else if(old_value != new_value)
-			output(Info,"%c[%s]%c/%s has changed from %s to %s",
-			       vi.scope.sigil,vi.xref.c_str(),vi.evalType,vi.key.c_str(),old_value.c_str(),new_value.c_str());
+			output(Info,"%c%s%c/%s has changed from %s to %s",
+			       vi.scope.sigil,xref.c_str(),vi.evalType,vi.key.c_str(),old_value.c_str(),new_value.c_str());
 		else
-			output(Info,"%c[%s]%c/%s is unchanged, current value: %s",
-			       vi.scope.sigil,vi.xref.c_str(),vi.evalType,vi.key.c_str(),old_value.c_str());
+			output(Info,"%c%s%c/%s is unchanged, current value: %s",
+			       vi.scope.sigil,xref.c_str(),vi.evalType,vi.key.c_str(),old_value.c_str());
 	}
 	else// Perform operation on set
 	{
