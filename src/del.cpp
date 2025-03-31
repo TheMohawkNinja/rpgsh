86f51f17fd72a39721a9bdb4c9229717ba83c7d0
0xbd <themohawkninja@aol.com>
2025-03-31 11:35:44 -0400

    Reworked for consistent syntax and fixed some issues

--- src/del.cpp
+++ src/del.cpp
@@ -17,7 +17,7 @@ int main(int argc, char** argv)
 	if(chkFlagHelp(argv))
 	{
 		fprintf(stdout,"USAGE:\n");
-		fprintf(stdout,"\tdel %svalue%s [%sOPTIONS%s]\n",TEXT_ITALIC,TEXT_NORMAL,TEXT_ITALIC,TEXT_NORMAL);
+		fprintf(stdout,"\tdel [%sOPTIONS%s] %svalue%s\n",TEXT_ITALIC,TEXT_NORMAL,TEXT_ITALIC,TEXT_NORMAL);
 		fprintf(stdout,"\nOPTIONS:\n");
 		fprintf(stdout,"\t%snone%s\t\tChecks %svalue%s against variables, variable sets, characters, and campaigns (in that order) and deletes the first match.\n",TEXT_ITALIC,TEXT_NORMAL,TEXT_ITALIC,TEXT_NORMAL);
 		fprintf(stdout,"\t-c\t\tDeletes character matching %svalue%s\n",TEXT_ITALIC,TEXT_NORMAL);
@@ -26,21 +26,25 @@ int main(int argc, char** argv)
 		return 0;
 	}
 
-	bool onlyChkC = false;
-	bool onlyChkM = false;
+	bool onlyChkC = !strcmp(argv[1],"-c");
+	bool onlyChkM = !strcmp(argv[1],"-m");
 
-	if(argv[2])
+	if((onlyChkC || onlyChkM) && argc == 2)
 	{
-		onlyChkC = !strcmp(argv[2],"-c");
-		onlyChkM = !strcmp(argv[2],"-m");
+		output(Error,"Missing argument for \"%s\".",argv[1]);
+		return -1;
+	}
+	else if(argv[1][0] == '-' && !onlyChkC && !onlyChkM)
+	{
+		output(Error,"Unknown option \"%s\".",argv[1]);
+		return -1;
 	}
 
-	std::string obj_to_be_deleted = std::string(argv[1]);
-
 	VariableInfo vi;
+	std::string obj_to_be_deleted = std::string(argv[1]);
 	if(looksLikeVariable(obj_to_be_deleted)) vi = parseVariable(obj_to_be_deleted);
 
-	if(!onlyChkC && !onlyChkM && vi.key != "" && !looksLikeSet(getSetStr(vi)) && looksLikeVariable(vi.variable))
+	if(!onlyChkC && !onlyChkM && looksLikeVariable(vi.variable) && vi.variable.back() != '/')
 	{
 		std::string value = getAppOutput(vi.variable).output[0];
 		bool deleted = false;
@@ -48,32 +52,33 @@ int main(int argc, char** argv)
 		if(vi.property != "")
 		{
 			output(Error,"Properties cannot be deleted.");
-			exit(-1);
+			return -1;
 		}
 
 		switch(vi.evalType)
 		{
 			case VAR_SIGIL:
-				if(vi.scope.remove<Var>(vi.key)) deleted = true;
+				deleted = vi.scope.remove<Var>(vi.key);
 				break;
 			case DICE_SIGIL:
-				if(vi.scope.remove<Dice>(vi.key)) deleted = true;
+				deleted = vi.scope.remove<Dice>(vi.key);
 				break;
 			case WALLET_SIGIL:
-				if(vi.scope.remove<Wallet>(vi.key)) deleted = true;
+				deleted = vi.scope.remove<Wallet>(vi.key);
 				break;
 			case CURRENCY_SIGIL:
-				if(vi.scope.remove<Currency>(vi.key)) deleted = true;
+				deleted = vi.scope.remove<Currency>(vi.key);
 				break;
 		}
 
 		vi.scope.save();
 		std::string xref = (vi.xref != "" ? "["+vi.xref+"]" : "");
-		if(deleted)	output(Warning,"Variable \"%c%s%c/%s\" (value: \"%s\") has been deleted.",vi.scope.sigil,xref.c_str(),vi.evalType,vi.key,value.c_str());
-		else		output(Error,"Variable \"%c%s%c/%s\" does not exist to be deleted.",vi.scope.sigil,xref.c_str(),vi.evalType,vi.key);
+		if(deleted)	output(Warning,"Variable \"%c%s%c/%s\" (value: \"%s\") has been deleted.",vi.scope.sigil,xref.c_str(),vi.evalType,vi.key.c_str(),value.c_str());
+		else		output(Error,"Variable \"%c%s%c/%s\" does not exist to be deleted.",vi.scope.sigil,xref.c_str(),vi.evalType,vi.key.c_str());
 	}
-	else if(!onlyChkC && !onlyChkM && vi.key != "" && looksLikeSet(getSetStr(vi)))
+	else if(!onlyChkC && !onlyChkM && vi.variable.back() == '/')
 	{
+		bool atLeastOneKeyRemoved = false;
 		struct RemovedKey
 		{
 			bool isRemoved = false;
@@ -105,22 +110,28 @@ int main(int argc, char** argv)
 
 			std::string xref = (vi.xref != "" ? "["+vi.xref+"]" : "");
 			if(rk.isRemoved)
+			{
 				output(Warning,"Variable \"%c%s%c/%s\" (value: \"%s\") has been deleted.",vi.scope.sigil,xref.c_str(),rk.type,k.c_str(),v.c_str());
+				atLeastOneKeyRemoved = true;
+			}
 		}
 
 		vi.scope.save();
+		if(!atLeastOneKeyRemoved)
+			output(Error,"Variable set \"%s\" appears to be empty.",vi.variable.c_str());
 	}
 	else if(onlyChkC || !onlyChkM)
 	{
 		Character c = Character();
 		std::string campaign_path;
-		if(findu(obj_to_be_deleted,'/') == std::string::npos)
+		std::string c_to_be_deleted = std::string(argv[2]);
+		if(findu(c_to_be_deleted,'/') == std::string::npos)
 			campaign_path = left(c.getDatasource(),rfindu(c.getDatasource(),'/')+1);
 		else
-			campaign_path = campaigns_dir + right(obj_to_be_deleted,findu(obj_to_be_deleted,'/')+1);
+			campaign_path = campaigns_dir + right(c_to_be_deleted,findu(c_to_be_deleted,'/')+1);
 		for(const auto& entry : getDirectoryListing(campaign_path))
 		{
-			if(stringcasecmp(entry,obj_to_be_deleted+".char") || std::filesystem::is_directory(campaign_path+entry)) continue;
+			if(stringcasecmp(entry,c_to_be_deleted+".char") || std::filesystem::is_directory(campaign_path+entry)) continue;
 
 			std::filesystem::remove(campaign_path+entry);
 			if(std::filesystem::exists(campaign_path+entry+".bak")) std::filesystem::remove(campaign_path+entry+".bak");
@@ -128,20 +139,21 @@ int main(int argc, char** argv)
 			return 0;
 		}
 
-		output(Error,"Character file \"%s.char\" does not exist to be deleted.",(campaign_path+obj_to_be_deleted).c_str());
+		output(Error,"Character file \"%s.char\" does not exist to be deleted.",(campaign_path+c_to_be_deleted).c_str());
 		return -1;
 	}
 	else
 	{
+		std::string m_to_be_deleted = std::string(argv[2]);
 		for(const auto& entry : getDirectoryListing(campaigns_dir))
 		{
-			if(stringcasecmp(entry,obj_to_be_deleted) || !std::filesystem::is_directory(campaigns_dir+entry)) continue;
+			if(stringcasecmp(entry,m_to_be_deleted) || !std::filesystem::is_directory(campaigns_dir+entry)) continue;
 			std::filesystem::remove_all(campaigns_dir+entry);
 			output(Warning,"Campaign directory \"%s\" has been deleted.",(campaigns_dir+entry).c_str());
 			return 0;
 		}
 
-		output(Error,"Campaign directory \"%s\" does not exist to be deleted.",(campaigns_dir+obj_to_be_deleted).c_str());
+		output(Error,"Campaign directory \"%s\" does not exist to be deleted.",(campaigns_dir+m_to_be_deleted).c_str());
 		return -1;
 	}
 
