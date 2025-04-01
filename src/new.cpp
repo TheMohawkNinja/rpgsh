a6ad3a7c7b49bf333379147f9c82187accff7156
0xbd <themohawkninja@aol.com>
2025-04-01 12:11:04 -0400

    Minor change to help text

--- src/new.cpp
+++ src/new.cpp
@@ -59,8 +59,8 @@ int main(int argc, char** argv)
 		fprintf(stdout,"USAGE:\n");
 		fprintf(stdout,"\tnew [%sOPTIONS%s]\n\n",TEXT_ITALIC,TEXT_NORMAL);
 		fprintf(stdout,"OPTIONS:\n");
-		fprintf(stdout,"\tnew -c %stemplate%s\tCreates a new character file using the character template %stemplate%s. Uses %sdefault_game%s setting if %stemplate%s is omitted.\n",TEXT_ITALIC,TEXT_NORMAL,TEXT_ITALIC,TEXT_NORMAL,TEXT_ITALIC,TEXT_NORMAL,TEXT_ITALIC,TEXT_NORMAL);
-		fprintf(stdout,"\tnew -m %sname%s\t\tCreates a new campaign file called %sname%s\n",TEXT_ITALIC,TEXT_NORMAL,TEXT_ITALIC,TEXT_NORMAL);
+		fprintf(stdout,"\t-c %stemplate%s\tCreates a new character file using the character template %stemplate%s. Uses %sdefault_game%s setting if %stemplate%s is omitted.\n",TEXT_ITALIC,TEXT_NORMAL,TEXT_ITALIC,TEXT_NORMAL,TEXT_ITALIC,TEXT_NORMAL,TEXT_ITALIC,TEXT_NORMAL);
+		fprintf(stdout,"\t-m %sname%s\t\tCreates a new campaign file called %sname%s\n",TEXT_ITALIC,TEXT_NORMAL,TEXT_ITALIC,TEXT_NORMAL);
 		return 0;
 	}
 
