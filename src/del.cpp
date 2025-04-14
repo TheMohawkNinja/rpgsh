d8b3a0eee7ebd2a4de402a9e671370b1ab1d5231
0xbd <themohawkninja@aol.com>
2025-04-14 14:48:21 -0400

    Fixed issues with campaign/character syntax

--- src/del.cpp
+++ src/del.cpp
@@ -19,7 +19,7 @@ int main(int argc, char** argv)
 		fprintf(stdout,"USAGE:\n");
 		fprintf(stdout,"\tdel [%sOPTIONS%s] %svalue%s\n",TEXT_ITALIC,TEXT_NORMAL,TEXT_ITALIC,TEXT_NORMAL);
 		fprintf(stdout,"\nOPTIONS:\n");
-		fprintf(stdout,"\t%snone%s\t\tChecks %svalue%s against variables, variable sets, characters, and campaigns (in that order) and deletes the first match.\n",TEXT_ITALIC,TEXT_NORMAL,TEXT_ITALIC,TEXT_NORMAL);
+		fprintf(stdout,"\t%snone%s\t\tDeletes the variable, or variable set specified by %svalue%s.\n",TEXT_ITALIC,TEXT_NORMAL,TEXT_ITALIC,TEXT_NORMAL);
 		fprintf(stdout,"\t-c\t\tDeletes character matching %svalue%s\n",TEXT_ITALIC,TEXT_NORMAL);
 		fprintf(stdout,"\t-m\t\tDeletes campaign matching %svalue%s\n",TEXT_ITALIC,TEXT_NORMAL);
 		fprintf(stdout,"\t%s | %s\tPrints this help text\n",FLAG_HELPSHORT,FLAG_HELPLONG);
@@ -44,92 +44,107 @@ int main(int argc, char** argv)
 	std::string obj_to_be_deleted = std::string(argv[1]);
 	if(looksLikeVariable(obj_to_be_deleted)) vi = parseVariable(obj_to_be_deleted);
 
-	if(!onlyChkC && !onlyChkM && looksLikeVariable(vi.variable) && vi.variable.back() != '/')
+	if(!onlyChkC && !onlyChkM)
 	{
-		std::string xref = (vi.xref != "" ? "["+vi.xref+"]" : "");
-		std::string value = getAppOutput(vi.variable).output[0];
-		bool deleted = false;
-
-		if(vi.property != "")
+		if(!looksLikeVariable(vi.variable) && vi.variable.back() != '/')
 		{
-			output(Error,"Properties cannot be deleted.");
+			output(Error,"\"%s\" does not look like a variable or variable set.",argv[1]);
 			return -1;
 		}
 
-		switch(vi.evalType)
+		if(vi.variable.back() != '/')
 		{
-			case VAR_SIGIL:
-				deleted = vi.scope.remove<Var>(vi.key);
-				break;
-			case DICE_SIGIL:
-				deleted = vi.scope.remove<Dice>(vi.key);
-				break;
-			case WALLET_SIGIL:
-				deleted = vi.scope.remove<Wallet>(vi.key);
-				break;
-			case CURRENCY_SIGIL:
-				deleted = vi.scope.remove<Currency>(vi.key);
-				break;
-		}
+			std::string xref = (vi.xref != "" ? "["+vi.xref+"]" : "");
+			std::string value = getAppOutput(vi.variable).output[0];
+			bool deleted = false;
 
-		vi.scope.save();
-		if(deleted)	output(Info,"Variable \"%c%s%c/%s\" (value: \"%s\") has been deleted.",vi.scope.sigil,xref.c_str(),vi.evalType,vi.key.c_str(),value.c_str());
-		else		output(Error,"Variable \"%c%s%c/%s\" does not exist to be deleted.",vi.scope.sigil,xref.c_str(),vi.evalType,vi.key.c_str());
-	}
-	else if(!onlyChkC && !onlyChkM && vi.variable.back() == '/')
-	{
-		std::string xref = (vi.xref != "" ? "["+vi.xref+"]" : "");
+			if(vi.property != "")
+			{
+				output(Error,"Properties cannot be deleted.");
+				return -1;
+			}
 
-		bool atLeastOneKeyRemoved = false;
-		struct RemovedKey
-		{
-			bool isRemoved = false;
-			char type = 0;
-		};
-		for(const auto& [k,v] : getSet(getSetStr(vi)))
-		{
-			RemovedKey rk;
-
-			switch(v[0])
+			switch(vi.evalType)
 			{
 				case VAR_SIGIL:
-					if(vi.type == '/' || vi.type == VAR_SIGIL)
-						rk = {vi.scope.remove<Var>(k),VAR_SIGIL};
+					deleted = vi.scope.remove<Var>(vi.key);
 					break;
 				case DICE_SIGIL:
-					if(vi.type == '/' || vi.type == DICE_SIGIL)
-						rk = {vi.scope.remove<Dice>(k),DICE_SIGIL};
+					deleted = vi.scope.remove<Dice>(vi.key);
 					break;
 				case WALLET_SIGIL:
-					if(vi.type == '/' || vi.type == WALLET_SIGIL)
-						rk = {vi.scope.remove<Wallet>(k),WALLET_SIGIL};
+					deleted = vi.scope.remove<Wallet>(vi.key);
 					break;
 				case CURRENCY_SIGIL:
-					if(vi.type == '/' || vi.type == CURRENCY_SIGIL)
-						rk = {vi.scope.remove<Currency>(k),CURRENCY_SIGIL};
+					deleted = vi.scope.remove<Currency>(vi.key);
 					break;
 			}
 
-			if(rk.isRemoved)
-			{
-				output(Info,"Variable \"%c%s%c/%s\" (value: \"%s\") has been deleted.",vi.scope.sigil,xref.c_str(),rk.type,k.c_str(),v.c_str());
-				atLeastOneKeyRemoved = true;
-			}
+			vi.scope.save();
+			if(deleted)	output(Info,"Variable \"%c%s%c/%s\" (value: \"%s\") has been deleted.",vi.scope.sigil,xref.c_str(),vi.evalType,vi.key.c_str(),value.c_str());
+			else		output(Error,"Variable \"%c%s%c/%s\" does not exist to be deleted.",vi.scope.sigil,xref.c_str(),vi.evalType,vi.key.c_str());
 		}
+		else
+		{
+			std::string xref = (vi.xref != "" ? "["+vi.xref+"]" : "");
 
-		vi.scope.save();
-		if(!atLeastOneKeyRemoved)
-			output(Error,"Variable set \"%s\" appears to be empty.",vi.variable.c_str());
+			bool atLeastOneKeyRemoved = false;
+			struct RemovedKey
+			{
+				bool isRemoved = false;
+				char type = 0;
+			};
+			for(const auto& [k,v] : getSet(getSetStr(vi)))
+			{
+				RemovedKey rk;
+
+				switch(v[0])
+				{
+					case VAR_SIGIL:
+						if(vi.type == '/' || vi.type == VAR_SIGIL)
+							rk = {vi.scope.remove<Var>(k),VAR_SIGIL};
+						break;
+					case DICE_SIGIL:
+						if(vi.type == '/' || vi.type == DICE_SIGIL)
+							rk = {vi.scope.remove<Dice>(k),DICE_SIGIL};
+						break;
+					case WALLET_SIGIL:
+						if(vi.type == '/' || vi.type == WALLET_SIGIL)
+							rk = {vi.scope.remove<Wallet>(k),WALLET_SIGIL};
+						break;
+					case CURRENCY_SIGIL:
+						if(vi.type == '/' || vi.type == CURRENCY_SIGIL)
+							rk = {vi.scope.remove<Currency>(k),CURRENCY_SIGIL};
+						break;
+				}
+
+				if(rk.isRemoved)
+				{
+					output(Info,"Variable \"%c%s%c/%s\" (value: \"%s\") has been deleted.",vi.scope.sigil,xref.c_str(),rk.type,k.c_str(),v.c_str());
+					atLeastOneKeyRemoved = true;
+				}
+			}
+
+			vi.scope.save();
+			if(!atLeastOneKeyRemoved)
+				output(Error,"Variable set \"%s\" appears to be empty.",vi.variable.c_str());
+		}
 	}
-	else if(onlyChkC || !onlyChkM)
+	else if(onlyChkC)
 	{
 		Character c = Character();
 		std::string campaign_path;
 		std::string c_to_be_deleted = std::string(argv[2]);
 		if(findu(c_to_be_deleted,'/') == std::string::npos)
+		{
 			campaign_path = left(c.getDatasource(),rfindu(c.getDatasource(),'/')+1);
+		}
 		else
-			campaign_path = campaigns_dir + right(c_to_be_deleted,findu(c_to_be_deleted,'/')+1);
+		{
+			campaign_path = campaigns_dir+left(c_to_be_deleted,findu(c_to_be_deleted,'/')+1)+"characters/";
+			c_to_be_deleted = right(c_to_be_deleted,findu(c_to_be_deleted,'/')+1);
+		}
+
 		for(const auto& entry : getDirectoryListing(campaign_path))
 		{
 			if(stringcasecmp(entry,c_to_be_deleted+".char") || std::filesystem::is_directory(campaign_path+entry)) continue;
