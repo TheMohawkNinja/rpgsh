6e4ee3adcf04c534a481020ca5c5d10e065d8717
TheMohawkNinja <themohawkninja@aol.com>
2025-04-29 21:04:54 -0400

    Continued development

--- src/valueof.cpp
+++ src/valueof.cpp
@@ -9,10 +9,10 @@ int main(int argc, char** argv)
 		exit(-1);
 	}
 	if(argc > 3)
-		output(Warning,"copy expects no more than 2 arguments, ignoring all other arguments");
+		output(Warning,"valueof expects no more than 2 arguments, ignoring all other arguments");
 
 	chkFlagAppDesc(argv,"Returns the value of the given wallet in the requested currency.");
-	chkFlagModifyVariables(argv,true);
+	chkFlagModifyVariables(argv,false);
 	if(chkFlagHelp(argv))
 	{
 		fprintf(stdout,"USAGE:\n");
@@ -25,7 +25,7 @@ int main(int argc, char** argv)
 	}
 
 	Wallet w_in, w_out;
-	//bool hasWallet = false;
+	bool hasWallet = false;
 	std::string currency;
 	for(int i=1; i<argc; i++)
 	{
@@ -34,7 +34,7 @@ int main(int argc, char** argv)
 			try
 			{
 				w_in = Wallet(std::string(argv[i]));
-				//hasWallet = true;
+				hasWallet = true;
 			}
 			catch(const std::runtime_error& e)
 			{
@@ -48,12 +48,35 @@ int main(int argc, char** argv)
 		}
 	}
 
+	if(!hasWallet)
+	{
+		output(Error,"valueof expects a wallet for an argument.");
+		return -1;
+	}
 	if(currency == "")
 	{
 		for(const auto& line : getAppOutput("print "+std::string(w_in)).output)
 			fprintf(stdout,"%s\n",line.c_str());
 		fprintf(stdout,"\b");
 		return 0;
+	}
+	else if(!w_in.containsCurrency(currency))
+	{
+		output(Error,"Currency \"%s\" is not contained within the inputted Wallet.",currency.c_str());
+		return -1;
+	}
+	else
+	{
+		Currency smallest;
+		for(const auto& [c,q] : w_in)
+		{
+			if(stringcasecmp(c.Name,currency)) continue;
+			smallest = c;
+			break;
+		}
+
+		//sdea = Smallest Denomination Equivalent Amount
+		unsigned int sdea = getEquivalentValueInLowestDenomination(smallest.System);
 	}
 
 	return 0;
