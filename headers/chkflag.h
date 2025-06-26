dc6101a3b28b2e76e3c153b633634dcf00c26ffa
TheMohawkNinja <themohawkninja@aol.com>
2025-06-26 18:53:33 -0400

    Removed

--- headers/chkflag.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <string>
-
-void chkFlagAppDesc(char** _argv, std::string description);
-void chkFlagModifyVariables(char** _argv, bool canModify=false);
-bool chkFlagHelp(char** _argv);
