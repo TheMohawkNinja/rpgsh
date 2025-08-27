e1a1ef27422031b6f73ee647dc5e87dca8a581fc
0xbd <themohawkninja@aol.com>
2025-08-27 15:17:27 -0400

    Initial commit

---
+++ src/if.cpp
@@ -0,0 +1,93 @@
+#include <cstring>
+#include "../headers/functions.h"
+#include "../headers/text.h"
+
+int main(int argc, char** argv)
+{
+	if(argc == 1)
+	{
+		output(error,"if expects at least 1 argument.");
+		return -1;
+	}
+
+	chkFlagAppDesc(argv,"Evaluates a condition, executing subsequent commands if the condition is true.");
+	chkFlagPreserveVariables(argv,all);
+
+	if(chkFlagHelp(argv))
+	{
+		fprintf(stdout,"USAGE:\n");
+		fprintf(stdout,"\tif %scondition%s { %scommand%s; }\n",TEXT_ITALIC,TEXT_NORMAL,TEXT_ITALIC,TEXT_NORMAL);
+		fprintf(stdout,"\tif [%sOPTIONS%s]\n",TEXT_ITALIC,TEXT_NORMAL);
+		fprintf(stdout,"\n");
+		fprintf(stdout,"\t%scondition%s\tA condition (as evaluated by the \"eval\" command), that if true, will execute the commands between the \'{\' and \'}\'.\n",TEXT_ITALIC,TEXT_NORMAL);
+		fprintf(stdout,"\t%scommand%s\t\tAn rpgsh command, to be executed if %scondition%s is true. There can be an arbitrary number of commands, each one terminated with a semicolon (;).\n",TEXT_ITALIC,TEXT_NORMAL,TEXT_ITALIC,TEXT_NORMAL);
+		fprintf(stdout,"\nOPTIONS:\n");
+		fprintf(stdout,"\t%s | %s\tPrint this help text.\n",FLAG_HELPSHORT,FLAG_HELPLONG);
+		return 0;
+	}
+
+	bool found_start = false;
+	bool found_end = false;
+	std::string condition;
+	std::vector<std::string> commands;
+	for(int i=1; i<argc; i++)
+	{
+		if(!found_start && strcmp(argv[i],"{"))
+		{
+			condition += std::string(argv[i])+" ";
+		}
+		else if(!found_start)
+		{
+			found_start = true;
+			condition.erase(condition.end()-1);
+		}
+		else if(!found_end && strcmp(argv[i],"}"))
+		{
+			std::string command;
+			for(int j=i; j<argc; i++, j++)
+			{
+				command += std::string(argv[j])+" ";
+				if(right(command,command.length()-2) == "; ")
+				{
+					command.erase(command.end()-2);
+					commands.push_back(command);
+					command = "";
+				}
+				else if(!strcmp(argv[j],"}"))
+				{
+					found_end = true;
+					break;
+				}
+			}
+		}
+		else if(!found_end)
+		{
+			found_end = true;
+			break;
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	if(!found_start)
+	{
+		output(error,"No commands specified.");
+		return -1;
+	}
+	if(!found_end)
+	{
+		output(error,"End of commands not found.");
+		return -1;
+	}
+
+	if(!stob(getAppOutput(condition).output[0])) return 0;
+
+	for(auto& cmd : commands)
+	{
+		runApp(handleBackslashEscSeqs(cmd),false);
+	}
+
+	return 0;
+}
