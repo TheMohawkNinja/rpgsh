d76538fc9581f9d74dff32739e58cbf668559ad1
TheMohawkNinja <themohawkninja@aol.com>
2025-08-28 18:53:44 -0400

    Implemented parseComplexCommandString()

--- src/if.cpp
+++ src/if.cpp
@@ -27,84 +27,11 @@ int main(int argc, char** argv)
 		return 0;
 	}
 
-	bool found_start = false;
-	bool found_end = false;
-	std::string condition, commands_str;
-	std::vector<std::string> commands;
-	for(int i=1; i<argc; i++)
-	{
-		if(!found_start && strcmp(argv[i],"{"))
-		{
-			condition += std::string(argv[i])+" ";
-		}
-		else if(!found_start)
-		{
-			found_start = true;
-			condition.erase(condition.end()-1);
-		}
-		else
-		{
-			for(int j=argc-1; j>=i; j--)
-			{
-				std::string j_arg = std::string(argv[j]);
-				if(found_end)
-					commands_str = j_arg+" "+commands_str;
-				else if(!found_end && j_arg.back() == '}')
-					found_end = true;
-			}
-			break;
-		}
-	}
+	ComplexCommandInfo cci = parseComplexCommandString(argc, argv, true);
 
-	commands = split(commands_str,';');
+	if(!stob(getAppOutput("eval "+cci.condition).output[0])) return 0;
 
-	for(unsigned i=0; i<commands.size(); i++)
-	{
-		if(countu(commands[i],'{') > countu(commands[i],'}'))
-		{
-			if(i == commands.size()-1)
-			{
-				output(error,"\t\'}\' not found.");
-				return -1;
-			}
-			else if(findu(commands[i+1],'}') != std::string::npos)
-			{
-				commands[i] += left(commands[i+1],findu(commands[i+1],'}')+1);
-				commands[i+1] = right(commands[i+1],findu(commands[i+1],'}')+1);
-				i=0;
-			}
-			else
-			{
-				commands[i] += ";"+commands[i+1]+";";
-				commands.erase(commands.begin()+i+1);
-				i=0;
-			}
-		}
-	}
-
-	//Cleaup with regex
-	for(auto& command : commands)
-	{
-		command = std::regex_replace(command,std::regex(";{2,}"),";");
-		command = std::regex_replace(command,std::regex("};"),"}");
-		if(findu(command,'{') != std::string::npos)
-			command = std::regex_replace(command,std::regex("\\\\"),"\\\\");
-	}
-
-	if(!found_start)
-	{
-		output(error,"No commands specified.");
-		return -1;
-	}
-	if(!found_end)
-	{
-		output(error,"End of commands not found.");
-		return -1;
-	}
-
-	if(!stob(getAppOutput("eval "+condition).output[0])) return 0;
-
-	for(auto& command : commands)
+	for(auto& command : cci.commands)
 	{
 		int status = runApp(handleBackslashEscSeqs(command),false);
 		if(status == STATUS_BREAK || status == STATUS_CONTINUE)
