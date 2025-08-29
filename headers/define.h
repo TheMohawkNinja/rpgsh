f364e4ce8b1292c5e5cf538624dd9eaa47a3e345
0xbd <themohawkninja@aol.com>
2025-08-29 15:10:04 -0400

    Added scripts_dr

--- headers/define.h
+++ headers/define.h
@@ -118,6 +118,7 @@ extern std::string configuration_path;
 extern std::string campaigns_dir;
 extern std::string templates_dir;
 extern std::string dice_lists_dir;
+extern std::string scripts_dir;
 extern std::string history_path;
 extern std::string prefix;
 extern std::string c_ext;
