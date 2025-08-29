e38b9d78854f7ae356a591e149b9b727cbf840c3
0xbd <themohawkninja@aol.com>
2025-08-29 08:26:44 -0400

    Added missing parts in help text and fixed issue key/value replacement

--- src/for.cpp
+++ src/for.cpp
@@ -20,8 +20,9 @@ int main(int argc, char** argv)
 		fprintf(stdout,"\tfor %skey%s,%svalue%s %sset%s { %scommand%s; }\n",TEXT_ITALIC,TEXT_NORMAL,TEXT_ITALIC,TEXT_NORMAL,TEXT_ITALIC,TEXT_NORMAL,TEXT_ITALIC,TEXT_NORMAL);
 		fprintf(stdout,"\tfor [%sOPTIONS%s]\n",TEXT_ITALIC,TEXT_NORMAL);
 		fprintf(stdout,"\n");
-		fprintf(stdout,"\t%skey%s\tArbitrary variable name for referencing the current key in the set. These are used in the for \"$/%skey%s.\n",TEXT_ITALIC,TEXT_NORMAL,TEXT_ITALIC,TEXT_NORMAL);
-		fprintf(stdout,"\t%svalue%s\tArbitrary variable name for referencing the current value in the set. These are used in the for \"$/%svalue%s.\n",TEXT_ITALIC,TEXT_NORMAL,TEXT_ITALIC,TEXT_NORMAL);
+		fprintf(stdout,"\t%skey%s\tArbitrary variable name for referencing the current key in the set. These are used in the form \"$/%skey%s\".\n",TEXT_ITALIC,TEXT_NORMAL,TEXT_ITALIC,TEXT_NORMAL);
+		fprintf(stdout,"\t%svalue%s\tArbitrary variable name for referencing the current value in the set. These are used in the form \"$/%svalue%s\".\n",TEXT_ITALIC,TEXT_NORMAL,TEXT_ITALIC,TEXT_NORMAL);
+		fprintf(stdout,"\t%sset%s\tThe variable set to be iterated through.\n",TEXT_ITALIC,TEXT_NORMAL);
 		fprintf(stdout,"\t%scommand%s\tAn rpgsh command, to be executed if %scondition%s is true. There can be an arbitrary number of commands, each one terminated with a semicolon (;).\n",TEXT_ITALIC,TEXT_NORMAL,TEXT_ITALIC,TEXT_NORMAL);
 		fprintf(stdout,"\nOPTIONS:\n");
 		fprintf(stdout,"\t%s | %s\tPrint this help text.\n",FLAG_HELPSHORT,FLAG_HELPLONG);
@@ -42,23 +43,28 @@ int main(int argc, char** argv)
 
 	if(findu(cci.condition,' ') == std::string::npos)
 	{
-		output(error,"Expected value name.");
+		output(error,"Expected value name after \',\'.");
 		return -1;
 	}
 	value = left(cci.condition,findu(cci.condition,' '));
 	set = right(cci.condition,value.length()+1);
 
 	vi = parseVariable(set);
-	Scope s = vi.scope;
-
-	for(auto& [k,v] : getSet(getSetStr(vi)))
+	set = getSetStr(vi);
+	if(!looksLikeSet(set))
 	{
-		for(auto& command : cci.commands)
+		output(error,"Expected set after value name.");
+		return -1;
+	}
+
+	for(auto& [k,v] : getSet(set))
+	{
+		for(const auto& command : cci.commands)
 		{
-			while(command.back() == ' ') command.erase(command.end());
-			command = std::regex_replace(command,std::regex("\\$/"+key),k);
-			command = std::regex_replace(command,std::regex("\\$/"+value),v);
-			int status = runApp(handleBackslashEscSeqs(command),false);
+			std::string cmd_to_run = command;
+			cmd_to_run = std::regex_replace(cmd_to_run,std::regex("\\$/"+key),k);
+			cmd_to_run = std::regex_replace(cmd_to_run,std::regex("\\$/"+value),v);
+			int status = runApp(handleBackslashEscSeqs(cmd_to_run),false);
 			if(status == STATUS_BREAK) return 0;
 			if(status == STATUS_CONTINUE) break;
 		}
