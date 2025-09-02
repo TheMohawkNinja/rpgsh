1c35cb36e87d6e91587678a0c4146c2e8716843c
0xbd <themohawkninja@aol.com>
2025-09-02 12:21:03 -0400

    Replaced references to subshells with command substitution

--- src/functions.cpp
+++ src/functions.cpp
@@ -1562,19 +1562,19 @@ int runApp(std::string arg_str, bool redirect_output)
 
 	std::sregex_iterator end;
 
-	//Handle subshells
-	std::regex subshell_pattern("\\$\\(((?!\\$\\()[^\\)])+?\\)+?");
-	std::sregex_iterator subshell_it(arg_str.begin(),arg_str.end(),subshell_pattern);
-	while(subshell_it != end)
+	//Handle command substitution
+	std::regex cmdsub_pattern("\\$\\(((?!\\$\\()[^\\)])+?\\)+?");
+	std::sregex_iterator cmdsub_it(arg_str.begin(),arg_str.end(),cmdsub_pattern);
+	while(cmdsub_it != end)
 	{
-		std::regex escaped_subshell_pattern(std::regex_replace(escapeRegexGroupChars(subshell_it->str()),std::regex("\\$"),"\\$"));
-		std::string subshell_output;
-		for(const auto& line : getAppOutput(right(left(subshell_it->str(),subshell_it->str().length()-1),2)).output)
-			subshell_output += line+"\n";
-		subshell_output = left(subshell_output,subshell_output.length()-2);
-		if(!Var(subshell_output).isInt()) subshell_output = "\""+regex_replace(subshell_output,std::regex("\\n"),"\\n")+"\"";
-		arg_str = std::regex_replace(arg_str,escaped_subshell_pattern,subshell_output,std::regex_constants::format_first_only);
-		subshell_it = std::sregex_iterator(arg_str.begin(),arg_str.end(),subshell_pattern);
+		std::regex escaped_cmdsub_pattern(std::regex_replace(escapeRegexGroupChars(cmdsub_it->str()),std::regex("\\$"),"\\$"));
+		std::string cmdsub_output;
+		for(const auto& line : getAppOutput(right(left(cmdsub_it->str(),cmdsub_it->str().length()-1),2)).output)
+			cmdsub_output += line+"\n";
+		cmdsub_output = left(cmdsub_output,cmdsub_output.length()-2);
+		if(!Var(cmdsub_output).isInt()) cmdsub_output = "\""+regex_replace(cmdsub_output,std::regex("\\n"),"\\n")+"\"";
+		arg_str = std::regex_replace(arg_str,escaped_cmdsub_pattern,cmdsub_output,std::regex_constants::format_first_only);
+		cmdsub_it = std::sregex_iterator(arg_str.begin(),arg_str.end(),cmdsub_pattern);
 	}
 
 	Configuration cfg = Configuration();
