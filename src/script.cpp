bb87dfa27c46502d8b5834fb9d04eb5ec186bef3
0xbd <themohawkninja@aol.com>
2025-09-03 14:50:05 -0400

    Can now list available scripts

--- src/script.cpp
+++ src/script.cpp
@@ -16,6 +16,7 @@ int main(int argc, char** argv)
 		fprintf(stdout,"\tscript [%sOPTIONS%s]\n",TEXT_ITALIC,TEXT_NORMAL);
 		fprintf(stdout,"\nOPTIONS:\n");
 		fprintf(stdout,"\t%sscript%s\t\tRuns the script %sscript%s.\n",TEXT_ITALIC,TEXT_NORMAL,TEXT_ITALIC,TEXT_NORMAL);
+		fprintf(stdout,"\t-l\t\tPrints all of the available scripts in the default folder.\n");
 		fprintf(stdout,"\t%s | %s\tPrint this help text.\n",FLAG_HELPSHORT,FLAG_HELPLONG);
 		return 0;
 	}
@@ -24,6 +25,13 @@ int main(int argc, char** argv)
 	{
 		output(error,"No rpgsh script specified.");
 		return -1;
+	}
+	else if(!strcmp(argv[1],"-l"))
+	{
+		std::vector<std::string> scripts = getDirectoryListing(scripts_dir);
+		for(const auto& script : scripts)
+			if(!std::filesystem::is_directory(scripts_dir+"/"+script)) fprintf(stdout,"%s\n",script.c_str());
+		return 0;
 	}
 
 	return runScript(std::string(argv[1]));
